77e276db5410ea844b8ffbe3f6420b1c499bb7c8
Shane Richards <shane.richards121@gmail.com>
2016-09-02 14:57:07 +0000

    completed

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/api/whoami', function(req, res) {
-    
+    var os = req.headers['user-agent'].match(/\(.*\)/)[0].split(/\)/)[0].substring(1);
+    res.json({
+        ipaddress: req.ip,
+        language: req.headers['accept-language'],
+        software: os
+        
+    })
 });
 
 app.listen(8080, function() {
